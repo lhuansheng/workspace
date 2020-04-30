339c01c4feabeb9d07fb53bc6681d6e1c4989da1
Charlie2244 <lhs2244@163.com>
2020-04-30 22:36:11 +0800

    react true

---
+++ lesson_xe/react/react/hello-react/src/CommentList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function CommentList(){
+    return (
+        <ul>
+            <li>
+                用户名: 大量的撒旦
+            </li>
+            <li>
+                用户名: 大量的撒旦
+            </li>
+            <li>
+                用户名: 大量的撒旦
+            </li>
+            <li>
+                用户名: 大量的撒旦
+            </li>
+            <li>
+                用户名: 大量的撒旦
+            </li>
+        </ul>
+    )
+}
+export default CommentList;
