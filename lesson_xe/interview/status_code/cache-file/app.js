3faad9854aff6e6e6ec6b4ff453be70eaa57b240
Charlie2244 <lhs2244@163.com>
2020-05-16 09:03:10 +0800

    还没弄清楚

--- lesson_xe/interview/status_code/cache-file/app.js
+++ lesson_xe/interview/status_code/cache-file/app.js
@@ -1,35 +1,21 @@
-const http = require('http')
-const fs = require('fs')
-const users = require('./user.json')
-// console.log(users)
-let version = 12345678
-let server = http.createServer((req,res)=>{
-  if(req.url == '/'){//如果请求的是首页的话
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
-    fs.createReadStream('./index.html').pipe(res)
-    return;
+const http = require('http');
+const fs = require('fs');
+const users = require('./users.json');
+// console.log(users);
+let version = 1;
+let server = http.createServer((req, res) => {
+  // If-None-Match
+  if (req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    fs.createReadStream('index.html').pipe(res);
+  } else if (req.url == '/users') {
+    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    // send 文本  Buffer 
+    // JSON 对象变成文本
+    // console.log(typeof JSON.stringify(users));
+    // res.setHeader('ETag', version);
+    res.end(JSON.stringify(users)) // 怎么把JSON 传过去? 
   }
-  else if(req.url =='/user'){
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
-    console.log(JSON.stringify(users))
-    res.end(JSON.stringify(users))
-  }
-  // if(req.headers['if-none-match']){
-  //   if(Number(req.headers['if-none-match'])==version){
-  //     res.statusCode = 304
-  //     res.end()
-  //     return;
-  //   }
-  //   // console.log(req.headers['If-None-Match'])
-  //   else{
-  //     res.setHeader('Etag',version)
-  //   res.end('hello node')
-  //   return
-  //   }
-  // }
+});
 
-  // res.setHeader('Etag','12345678')
-  // res.end('hello world')
-})
-
-server.listen(3000)
+server.listen(3000);
