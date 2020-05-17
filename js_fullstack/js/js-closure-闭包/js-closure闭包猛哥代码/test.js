c775a5024138c54830282a0c0eeab96a141ae9d5
Charlie2244 <lhs2244@163.com>
2020-05-17 15:22:58 +0800

    etag 如果数据未修改，前端再次请求不需再返数据 304，告诉前端从localstroge 拿数据

---
+++ js_fullstack/js/js-closure-闭包/js-closure闭包猛哥代码/test.js
@@ -0,0 +1,17 @@
+// let i=1
+// function f(){
+//   for(i;i<10;i++){
+// console.log(i)
+//   }
+// }
+// f()
+// console.log(i)
+foo()
+// var foo=  function(){
+// console.log(112)
+// }
+function foo(){
+  console.log(a)
+  var a=2
+}
+
