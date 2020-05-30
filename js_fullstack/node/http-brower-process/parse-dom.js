931a014ce5aaf66490c282c73814dadeda33daa9
Charlie2244 <lhs2244@163.com>
2020-05-30 16:51:35 +0800

    parse-dom

--- js_fullstack/node/http-brower-process/parse-dom.js
+++ js_fullstack/node/http-brower-process/parse-dom.js
@@ -1,11 +1,39 @@
 
 
-let htmlStr=`<html><head></head><body><div></div></body></html>`
+let htmlStr=`<html><head></head><body><img /><div></div></body></html>`
 // token: html tag
 let currentToken = null
+
+let stack = [{type:'document',children:[]}]
 parse(htmlStr)
+console.log(JSON.stringify(stack[0],null,2))
 function emit(token){
-  console.log(token)
+  // console.log(token)
+  let top = stack[stack.length - 1]
+  if(token.type === 'startTag'){
+    let element = {
+      type:'element',
+      children:[],
+      attributes:[],
+      tagName:token.tagName
+    }
+    stack.push(element)
+    top.children.push(element)
+  } else if(token.type === 'endTag'){
+    if(token.tagName !=top.tagName){
+      throw new Error('tagname match error')
+    }else{
+      stack.pop()
+    }
+  }
+  else if(token.type === 'selfCloseToken'){
+    let element = {
+      type:'element',
+      children:[],
+      attributes:[],
+      tagName:token.tagName
+    }
+  }
   currentToken = null
 }
 function parse(htmlString){
@@ -29,6 +57,7 @@ function tagOpen(c){
   if(c==='/'){
     return endTagOpen
   }
+ 
   else if(c.match(/^[a-zA-Z]$/)){
     currentToken = {
       type:'startTag',
@@ -41,10 +70,20 @@ function tagName(c){
   if(c.match(/^[a-zA-Z]$/)){
     currentToken.tagName +=c
     return tagName
-  }else if(c === '>'){
+  }
+  else if(c.match(/[\t\n\f ]/)){
+    return beforeAttaributeName
+  }
+  else if(c === '>'){
     // tag 拼接结束
     emit(currentToken)
     return start
+  }
+}
+function beforeAttaributeName(c){
+  if(c === '/'){
+    currentToken.type = 'selfCloseToken'
+    return tagName
   }
 }
 function endTagOpen(c){
