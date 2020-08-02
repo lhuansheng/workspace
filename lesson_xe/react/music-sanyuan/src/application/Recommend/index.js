6cdfd062e15b54b170534d5390f4efe453a7e45f
Charlie2244 <lhs2244@163.com>
2020-08-02 22:15:45 +0800

    解析三元  列表

--- lesson_xe/react/music-sanyuan/src/application/Recommend/index.js
+++ lesson_xe/react/music-sanyuan/src/application/Recommend/index.js
@@ -1,35 +1,53 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actionTypes from "./store/actionCreators";
-import Slider from '../../components/slider/';
-import { Content } from './style';
+import Slider from "../../components/slider/";
+import { Content } from "./style";
+import { forceCheck } from 'react-lazyload';
+import Scroll from '../../baseUI/scroll/index';
+import RecommendList from '../../components/list/';
 function Recommend(props) {
-  const { bannerList } = props;
-  const { getBannerDataDispatch } = props;
+  const { bannerList, recommendList, enterLoading } = props;
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   useEffect(() => {
     if (!bannerList.size) {
       getBannerDataDispatch();
     }
+    if(!recommendList.size){
+      getRecommendListDataDispatch();
+    }
     // eslint-disable-next-line
   }, []);
 
   const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() :[];
   return (
-    <Content >
-      <Slider bannerList={bannerListJS}></Slider>
+    <Content>
+       <Scroll className="list" onScroll={forceCheck}>
+      <div>
+        <Slider bannerList={bannerListJS}></Slider>
+        <RecommendList recommendList={recommendListJS}></RecommendList>
+      </div>
+      </Scroll>
     </Content>
   );
 }
 // 映射Redux全局的state到组件的props上
+// recommend bannerList  是对象路径，在recommend.recommendList
 const mapStateToProps = (state) => ({
   bannerList: state.getIn(["recommend", "bannerList"]),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
 });
 // 映射dispatch到props上
 const mapDispatchToProps = (dispatch) => {
   return {
     getBannerDataDispatch() {
       dispatch(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
     },
   };
 };
--- lesson_xe/react/music-sanyuan/src/application/Recommend/store/actionCreators.js
+++ lesson_xe/react/music-sanyuan/src/application/Recommend/store/actionCreators.js
@@ -1,11 +1,20 @@
 import * as actionTypes from './constants';
 import { fromJS } from 'immutable';
-import { getBannerRequest } from '../../../api/request';
+import { getBannerRequest, getRecommendListRequest } from '../../../api/request';
 
 
 export const changeBannerList = (data) => ({
   type: actionTypes.CHANGE_BANNER,
   data: fromJS(data)
+});
+export const changeRecommendList = (data) => ({
+  type: actionTypes.CHANGE_RECOMMEND_LIST,
+  data: fromJS(data)
+});
+
+export const changeEnterLoading = (data) => ({
+  type: actionTypes.CHANGE_ENTER_LOADING,
+  data
 });
 // 获取 轮播图数据
 export const getBannerList = () => {
@@ -16,5 +25,15 @@ export const getBannerList = () => {
     }).catch(() => {
       console.log("轮播图数据传输错误");
     }) 
+  }
+};
+export const getRecommendList = () => {
+  return (dispatch) => {
+    getRecommendListRequest().then(data => {
+      dispatch(changeRecommendList(data.result));
+      dispatch(changeEnterLoading(false));
+    }).catch(() => {
+      console.log("推荐歌单数据传输错误");
+    });
   }
 };
