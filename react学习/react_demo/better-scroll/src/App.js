53f328dd627c85e15dcd579f8fedaa22e98e1539
Charlie2244 <lhs2244@163.com>
2020-08-04 21:17:16 +0800

    better scroll 下拉刷新

--- react学习/react_demo/better-scroll/src/App.js
+++ react学习/react_demo/better-scroll/src/App.js
@@ -30,21 +30,38 @@ export default function App() {
   const scroll = useRef(null);
  
   useEffect(() => {
-    new BScroll('.scroll-wrapper', {
+   let scroll =  new BScroll('.scroll-wrapper', {
       scrollX:true,
-      scrollY: true,
       click: true,
       pullDownRefresh: {
         threshold: 90,
             stop: 0
       },
-      probeType: 3 // listening scroll hook
+      probeType: 0 // listening scroll hook
     });
+    let val = document.getElementsByClassName('top')[0]
+    scroll.on('scroll',(position)=>{
+      console.log(position)
+      if(position.y>=90){
+        val.innerHTML='松手刷新'
+      }
+      else{
+        val.innerHTML='下拉刷新'
+      }
+    })
+    scroll.on('touchEnd',(position)=>{
+      console.log(position,'aaaaaaa')
+      if(position.y>=90){
+       alert('数据刷新成功')
+      }
+    })
   });
+
   return (
-    <div class="core-container">
-      <div class="scroll-wrapper" ref={scroll}>
-        <div class="scroll-content">
+    <div className="core-container">
+      <div className="scroll-wrapper" ref={scroll}>
+        <div className="scroll-content">
+          <div className="top">下拉刷新</div>
           {data.map((item, i) => {
             return (
               <div className="scroll-item" key={i}>
