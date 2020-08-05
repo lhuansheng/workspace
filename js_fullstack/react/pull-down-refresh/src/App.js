2bb8442823b99a8d888444b866e3199a911a158a
Charlie2244 <lhs2244@163.com>
2020-08-05 21:27:57 +0800

    递归 模板字符串高级用法

--- js_fullstack/react/pull-down-refresh/src/App.js
+++ js_fullstack/react/pull-down-refresh/src/App.js
@@ -1,9 +1,41 @@
-import React,{ useEffect, useRef } from 'react';
+import React,{ useEffect, useRef,useState } from 'react';
 import './App.css';
 import pullToRefresh from './pullDown'
+import styled, { css } from 'styled-components'
 const { init } = pullToRefresh()
+// 标签模板
+function foo(...args) {
+  console.log(args)
+  let str = 'insert here'
+  str += args[0][0]
+  str += args[1]
+  str += args[0][1]
+  str += args[2]
+  str += args[0][2]
+  return str
+}
+console.log(foo` i am a ${18}, i am a boy ${19} `)
+const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: blue;
+  color: white;
+  border: 2px solid white;
+
+  /* The GitHub button is a primary button
+   * edit this to target it specifically! */
+  ${props => props.primary && css`
+    background: red;
+    color: black;
+  `}
+`
 
 function App() {
+  const [isPrimary, setIsPrimary] = useState(false)
   const contentRef = useRef()
   const ptrRef = useRef()
   const bodyRef = useRef()
@@ -28,6 +60,7 @@ function App() {
         <div className="loading" />
       </div>
       <header ref={contentRef} className="content-wrap">
+        <Button primary={isPrimary} onClick={ () => setIsPrimary(!isPrimary)} >按钮</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
