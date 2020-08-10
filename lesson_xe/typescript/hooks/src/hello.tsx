17e84cf9c4ac404fa004dfc9c5310cfe1a65505d
Charlie2244 <lhs2244@163.com>
2020-08-10 14:01:48 +0800

    compare old class hooks

---
+++ lesson_xe/typescript/hooks/src/hello.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+interface Props {
+    userName : string;
+   }
+export const HelloComponent = (props: Props) => (
+<h2>Hello user: { props.userName } !</h2>
+ );
---
+++ lesson_xe/typescript/readme.md
@@ -0,0 +1,9 @@
+1. 传统开发
+2. hooks 开发 
+
+class -> hooks 过渡
+- devDependencies dependencies
+  env development  test(docker)
+  babel 6 -> 7
+  webpack 4
+
