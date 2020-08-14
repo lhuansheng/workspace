0b1bc21b672f15a2a5f43b67ef18b6f405cac034
Charlie2244 <lhs2244@163.com>
2020-08-14 17:30:06 +0800

    babel webpack

---
+++ js_fullstack/js/babel/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+const sum = 2 ** 2
+function obsever() {
+  
+}
+@obsever
+class App extends Component {
+  render() { 
+    return ( 
+    <div>小明<img src="baidu.com" alt="baidu" className="box" /></div> 
+    );
+  }
+}
+ 
+export default App;
---
+++ js_fullstack/js/babel/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+       小马哥打裂缝
+    </div>
+  )
+}
+
+export default App
