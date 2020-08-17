1e58618d40e92b7db1ebd79bd2fb6c8641a86963
Charlie2244 <lhs2244@163.com>
2020-08-17 22:28:19 +0800

    blog start

---
+++ Nextdemo/blog/blog/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import 'antd/dist/antd.css'
+import '../public/pages/common.css'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
