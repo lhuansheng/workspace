4cc87e97d95eb3d91e98bed90cc2e3d367a2334b
Charlie2244 <lhs2244@163.com>
2020-08-19 13:26:23 +0800

    加上mysql数据存储

--- Nextdemo/blog/blog/pages/detailed.js
+++ Nextdemo/blog/blog/pages/detailed.js
@@ -9,7 +9,7 @@ import "../styles/pages/detailed.css";
 import ReactMarkdown from "react-markdown";
 import MarkNav from "markdown-navbar";
 import "markdown-navbar/dist/navbar.css";
-
+import axios from "axios";
 
 let markdown =
   "# P01:课程介绍和环境搭建\n" +
@@ -62,61 +62,79 @@ let markdown =
   ">> bbbbbbbbb\n" +
   ">>> cccccccccc\n\n" +
   "``` var a=11; ```";
-const Detailed = () => (
-  <>
-    <Head>
-      <title>博客详细页</title>
-    </Head>
-    <Header />
-    <Row className="comm-main" type="flex" justify="center">
-      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+const Detailed = (list) =>{
+  console.log(list)
+  return <>
+  <Head>
+<title>{list.data.title}</title>
+  </Head>
+  <Header />
+  <Row className="comm-main" type="flex" justify="center">
+    <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+      <div>
+        <div className="bread-div">
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <a href="/">首页</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+<Breadcrumb.Item>{list.data.title}</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+
         <div>
-          <div className="bread-div">
-            <Breadcrumb>
-              <Breadcrumb.Item>
-                <a href="/">首页</a>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-              <Breadcrumb.Item>xxxx</Breadcrumb.Item>
-            </Breadcrumb>
+          <div className="detailed-title">
+            React实战视频教程-技术胖Blog开发(更新08集)
           </div>
 
-          <div>
-            <div className="detailed-title">
-              React实战视频教程-技术胖Blog开发(更新08集)
-            </div>
+          <div className="list-icon center">
+            <span> 2019-06-28</span>
+            <span> 视频教程</span>
+            <span> 5498人</span>
+          </div>
 
-            <div className="list-icon center">
-              <span> 2019-06-28</span>
-              <span> 视频教程</span>
-              <span> 5498人</span>
-            </div>
-
-            <div className="detailed-content">
-              <ReactMarkdown source={markdown} escapeHtml={false} />
-            </div>
+          <div className="detailed-content">
+            <ReactMarkdown source={markdown} escapeHtml={false} />
           </div>
         </div>
-      </Col>
+      </div>
+    </Col>
 
-      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-        <Author />
-        <Advert />
-        <Affix offsetTop={5}>
-  <div className="detailed-nav comm-box">
-    <div className="nav-title">文章目录</div>
-    <MarkNav
-      className="article-menu"
-      source={markdown}
+    <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+      <Author />
+      <Advert />
+      <Affix offsetTop={5}>
+<div className="detailed-nav comm-box">
+  <div className="nav-title">文章目录</div>
+  <MarkNav
+    className="article-menu"
+    source={markdown}
 
-      ordered={false}
-    />
-  </div>
+    ordered={false}
+  />
+</div>
 </Affix>
-      </Col>
-    </Row>
-    <Footer />
-  </>
-);
+    </Col>
+  </Row>
+  <Footer />
+</>
+} 
+Detailed.getInitialProps = async(context)=>{
+
+  console.log(context.query.id)
+  let id =context.query.id
+  const promise = new Promise((resolve)=>{
+
+    axios('http://127.0.0.1:7001/default/getArticleById/'+id).then(
+      (res)=>{
+        console.log(res.data)
+        // resolve(res.data.data[0])
+        resolve(res.data)
+      }
+    )
+  })
+
+  return await promise
+}
 
 export default Detailed;
